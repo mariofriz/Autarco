547fddba2931cbddbe9685d26e41f236d916504c
Mario <evokow@gmail.com>
2015-11-23 00:30:53 +0000

    New button styles & prepared for ajax

--- js/app.js
+++ js/app.js
@@ -6,18 +6,29 @@ new Vue({
                 id: 0,
                 name: "Potager",
                 isWatering: true,
-                isLit: true
+                isLit: true,
+                wateringButton: true,
+                lightButton: true,
             },
             { 
                 id: 1,
                 name: "Jardin",
                 isWatering: false,
-                isLit: false
+                isLit: false,
+                wateringButton: true,
+                lightButton: true,
             }
         ],
         system: {
             counter: 200
-        }
+        },
+        events: [
+            {
+                zone: "Potager",
+                percent: 33,
+            },
+        ],
+        volume : ''
     },
     computed: {
         isPumping: function() {
@@ -30,16 +41,65 @@ new Vue({
             return false;
         }
     },
+    ready: function() {
+        this.update();
+        setInterval(this.update, 3000);
+    },
     methods: {
-        toggleLight: function(zoneId) {
-            this.zones[zoneId].isLit = !this.zones[zoneId].isLit;
+        update: function() {
+            // Get new data from service
+            $.get("/autarco/update", function(data, status){
+                alert("Data: " + data + "\nStatus: " + status);
+                this.updateUI(data);
+            });
         },
-        showModal: function(zoneId) {
-            $('#modal-' + zoneId).modal('show');
+        updateUI: function(data) {
+            // Update local data
+            this.zones = data.zones;
+            this.system = data.system;
+            this.events = data.events;
+            // Update progress bars
+            $('.progress').progress();
         },
-        confirmWatering: function(zoneId, volume) {
-            this.zones[zoneId].isWatering = !this.zones[zoneId].isWatering;
+        toggleLight: function(zone) {
+            var data = {
+                zoneId: zone.id,
+            };
+            // Post Request to toggle light
+            $.post("/autarco/light", data, function(data, status){
+                alert("Data: " + data + "\nStatus: " + status);
+                // Update UI
+                this.updateUI(data);
+
+                // Once light has been switched, turn button back on
+                zone.lightButton = true;
+            });
+            // Deactive light switch
+            zone.lightButton = false;
+            
+        },
+        showModal: function(zone) {
+            $('#modal-' + zone.id).modal('show');
+        },
+        confirmWatering: function(zone, volume) {
+            if (volume > 0) {
+                var data = {
+                    zoneId: zone.id,
+                    volume: volume
+                };
+                $.post("/autarco/water", data, function(data, status){
+                    alert("Data: " + data + "\nStatus: " + status);
+                    // Update UI
+                    this.updateUI(data);
+
+                    // Once event is registered reactivate button
+                    zone.wateringButton = true;
+                });
+                // Deactivate watering button
+                zone.wateringButton = false;
+            }
             // Water those !
+            this.volume = '';
         },
     }
 });
